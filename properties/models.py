5062562f42b0537b3da672a35087cfa2023e132d
AdnanK1 <adnangitonga@gmail.com>
2024-12-11 11:48:01 +0300

    Changed the ordering of the properties

--- properties/models.py
+++ properties/models.py
@@ -35,6 +35,9 @@ class Property(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
+    class Meta:
+        ordering = ['-created_at']
+
     def __str__(self):
         return f"{self.name} - {self.address}"
     
